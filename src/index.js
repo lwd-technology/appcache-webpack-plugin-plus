ac6098241ecf7179b19bbb160d38638b16f749be
Nick Babenko <nickbabenko@me.com>
2016-05-13 12:55:02 +0100

    added chunk hash support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 class AppCache {
 
-  constructor(cache, network, fallback, settings, hash, comment) {
+  constructor(cache, network, fallback, settings, hash, comment, chunkHash) {
     this.cache = cache;
     this.network = network;
     this.fallback = fallback;
@@ -8,6 +8,7 @@ class AppCache {
     this.hash = hash;
     this.comment = comment;
     this.assets = [];
+    this.chunkHash = chunkHash;
   }
 
   addAsset(asset) {
@@ -20,23 +21,23 @@ class AppCache {
 
   getManifestBody() {
     return [
-      this.assets && this.assets.length ? `${this.assets.join('\n')}\n` : null,
-      this.cache && this.cache.length ? `CACHE:\n${this.cache.join('\n')}\n` : null,
-      this.network && this.network.length ? `NETWORK:\n${this.network.join('\n')}\n` : null,
-      this.fallback && this.fallback.length ? `FALLBACK:\n${this.fallback.join('\n')}\n` : null,
-      this.settings && this.settings.length ? `SETTINGS:\n${this.settings.join('\n')}\n` : null,
-    ].filter(v => v && v.length).join('\n');
+          this.assets && this.assets.length ? `${this.assets.join('\n')}\n` : null,
+          this.cache && this.cache.length ? `CACHE:\n${this.cache.join('\n')}\n` : null,
+          this.network && this.network.length ? `NETWORK:\n${this.network.join('\n')}\n` : null,
+          this.fallback && this.fallback.length ? `FALLBACK:\n${this.fallback.join('\n')}\n` : null,
+          this.settings && this.settings.length ? `SETTINGS:\n${this.settings.join('\n')}\n` : null,
+        ].filter(v => v && v.length).join('\n');
   }
-  
+
   prepareCache() {
     this.cache = this.cache.map(asset => {
-      return asset.replace('{hash}', this.hash);
-    });
+          return asset.replace('{hash}', this.hash).replace('{chunkhash}', this.chunkHash);
+  });
   }
 
   source() {
     this.prepareCache();
-    
+
     return [
       'CACHE MANIFEST',
       `# ${this.hash}`,
@@ -51,13 +52,13 @@ export default class AppCachePlugin {
   static AppCache = AppCache
 
   constructor({
-    cache,
-    network = ['*'],
-    fallback,
-    settings,
-    exclude = [],
-    output = 'manifest.appcache',
-    comment,
+      cache,
+      network = ['*'],
+      fallback,
+      settings,
+      exclude = [],
+      output = 'manifest.appcache',
+      comment,
   } = {}) {
     this.cache = cache;
     this.network = network;
@@ -68,9 +69,9 @@ export default class AppCachePlugin {
 
     // Convert exclusion strings to RegExp.
     this.exclude = exclude.map(exclusion => {
-      if (exclusion instanceof RegExp) return exclusion;
-      return new RegExp(`^${exclusion}$`);
-    });
+          if (exclusion instanceof RegExp) return exclusion;
+    return new RegExp(`^${exclusion}$`);
+  });
   }
 
   apply(compiler) {
@@ -78,12 +79,12 @@ export default class AppCachePlugin {
     const {publicPath = ''} = outputOptions;
 
     compiler.plugin('emit', (compilation, callback) => {
-      const appCache = new AppCache(this.cache, this.network, this.fallback, this.settings, compilation.hash, this.comment);
-      Object.keys(compilation.assets)
+      const appCache = new AppCache(this.cache, this.network, this.fallback, this.settings, compilation.hash, this.comment, compilation.chunkhash);
+    Object.keys(compilation.assets)
         .filter(asset => !this.exclude.some(pattern => pattern.test(asset)))
-        .forEach(asset => appCache.addAsset(publicPath + asset));
-      compilation.assets[this.output] = appCache;
-      callback();
-    });
+  .forEach(asset => appCache.addAsset(publicPath + asset));
+    compilation.assets[this.output] = appCache;
+    callback();
+  });
   }
 }
